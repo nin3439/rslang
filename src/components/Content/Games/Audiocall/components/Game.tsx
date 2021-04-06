74a3976c3b72ff5f985b7f099ab46f97aecf6f96
nin3439 <ninok33_94@mail.ru>
2021-04-06 18:57:02 +0300

    feat: add basic layout of game

--- src/components/Content/Games/Audiocall/components/Game.tsx
+++ src/components/Content/Games/Audiocall/components/Game.tsx
@@ -19,6 +19,37 @@ const StyledIconButton = styled(IconButton)`
   }
 `;
 
+const StyledButtonResponses = styled(Button)`
+  background: #2b4054;
+  &.MuiButton-root {
+    color: #fff;
+  }
+  transform: scale(1);
+  transition: all 0.5s;
+  &:hover {
+    background: #2b4054;
+    transform: scale(1.1);
+    transition: transform 0.5s;
+  }
+`;
+
+const StyledButtonAudio = styled(Button)`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  margin-bottom: 100px;
+`;
+
+const StyledVolumeUp = styled(VolumeUp)`
+  width: 100px;
+  height: 100px;
+  transition: all 0.5s;
+  &:hover {
+    color: #2b4054;
+    transition: all 0.5s;
+  }
+`;
+
 interface IGameProps {
   setIsGameStart: (isGameStart: boolean) => void;
   setRightAnswers: (rightAnswers: any) => void;
@@ -34,6 +65,7 @@ export const Game: React.FC<IGameProps> = ({
   const [randomWord, setRandomWord] = useState<IWord | null>(null);
   const [playedWords, setPlayedWords] = useState<string[]>([]);
   const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [responseOptions, setResponseOptions] = useState<string[]>([]);
 
   const [playEnglishWord] = useSound(
     `https://dream-react-rslang-server.herokuapp.com/${randomWord?.audio}`
@@ -54,35 +86,74 @@ export const Game: React.FC<IGameProps> = ({
   }, [isDataLoaded]);
 
   const getRandomWord = () => {
-    if (words.length) {
-      let randomWord = words[Math.floor(Math.random() * words.length)];
-      while (playedWords.includes(randomWord.word)) {
-        randomWord = words[Math.floor(Math.random() * words.length)];
-      }
-      setWords((prev: IWord[]) =>
-        prev.filter((word: IWord) => word.id !== randomWord.id)
-      );
-      setPlayedWords((prev: string[]) => [...prev, randomWord.word]);
-      setRandomWord(randomWord);
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    while (playedWords.includes(randomWord.word)) {
+      randomWord = words[Math.floor(Math.random() * words.length)];
     }
+    setPlayedWords((prev: string[]) => [...prev, randomWord.word]);
+    setRandomWord(randomWord);
+    getResponseOptions(randomWord);
   };
 
-  console.log(randomWord);
+  const getResponseOptions = (randomWord: IWord) => {
+    let arrResponse = [randomWord?.wordTranslate];
+    for (let i = 0; i < 4; i++) {
+      let randomResponse = words[Math.floor(Math.random() * words.length)];
+      console.log(arrResponse, randomResponse.wordTranslate);
+      while (arrResponse.includes(randomResponse.wordTranslate)) {
+        randomResponse = words[Math.floor(Math.random() * words.length)];
+      }
+      arrResponse = [...arrResponse, randomResponse.wordTranslate];
+    }
+    arrResponse.sort(() => Math.random() - 0.5);
+    setResponseOptions(arrResponse);
+  };
 
   return (
-    <Grid>
-      <StyledIconButton
-        onClick={() => {
-          setIsGameStart(false);
-          setRightAnswers([]);
-        }}
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="flex-start"
+      style={{ height: '100vh' }}
+    >
+      <Grid container alignItems="center" justify="flex-start">
+        <StyledIconButton
+          onClick={() => {
+            setIsGameStart(false);
+            setRightAnswers([]);
+          }}
+        >
+          <ArrowBack fontSize="large" />
+        </StyledIconButton>
+      </Grid>
+
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ flex: 1 }}
       >
-        <ArrowBack fontSize="large" />
-      </StyledIconButton>
-      <Grid>
-        <Button variant="contained" onClick={() => playEnglishWord()}>
-          <VolumeUp />
-        </Button>
+        <StyledButtonAudio
+          variant="contained"
+          onClick={() => {
+            playEnglishWord();
+          }}
+        >
+          <StyledVolumeUp />
+        </StyledButtonAudio>
+        <Grid container alignItems="center" justify="center" spacing={3}>
+          {responseOptions.map((response, index) => {
+            return (
+              <Grid item key={response}>
+                <StyledButtonResponses variant="outlined">
+                  {index + 1} {response}{' '}
+                </StyledButtonResponses>
+              </Grid>
+            );
+          })}
+        </Grid>
       </Grid>
     </Grid>
   );
